e0f5dc5640a9952a16abe0dd9a9c7272aeacc23f
Marcos Oto Picarelli Prado <opicarelli@gmail.com>
2015-09-06 12:48:44 -0300

    Organizando grid

--- js/custom-script.js
+++ js/custom-script.js
@@ -28,56 +28,14 @@ $(function() {
     wall.fitWidth();
     
     $('#top-nav').slimmenu({
-	    resizeWidth: '800',
-	    collapserTitle: 'Menu',
-	    animSpeed: 'medium',
-	    easingEffect: null,
-	    indentChildren: false,
-	    childrenIndenter: '&nbsp;'
-	});
-	
-    $("a.fullscreenExit").hide();
-	$("a.fullscreen").on('click', function() {
-        var docElement, request;
-    
-        docElement = document.documentElement;
-        request = docElement.requestFullScreen || docElement.webkitRequestFullScreen || docElement.mozRequestFullScreen || docElement.msRequestFullScreen;
-    
-        if(typeof request!="undefined" && request){
-            request.call(docElement);
-        }
-        $(this).hide();
-        $('a.fullscreenExit').show();
-        return false;
+        resizeWidth: '800',
+        collapserTitle: 'Menu',
+        animSpeed: 'medium',
+        easingEffect: null,
+        indentChildren: false,
+        childrenIndenter: '&nbsp;'
     });
-    
-    $("a.fullscreenExit").on('click', function() {
-        var docElement, request;
-    
-        docElement = document;
-        request = docElement.cancelFullScreen|| docElement.webkitCancelFullScreen || docElement.mozCancelFullScreen || docElement.msCancelFullScreen || docElement.exitFullscreen;
-        if(typeof request!="undefined" && request){
-            request.call(docElement);
-        }
-        $(this).hide();
-        $('a.fullscreen').show();
-        return false;
-    });
-    
-    $('#open-iframe').hide();
-    $('a.open-project').click(function(){
-        $("body#parent").addClass("noscroll");
-        $('#open-iframe').show();
-        $("iframe").addClass("iframestyle");
-        submitComment();
-    });
-    
-    $("a.closeframe").click(function() {
-        $("body#parent", window.parent.document).removeClass("noscroll");
-        $('iframe').attr('src', $('iframe').attr('src'));
-        $('#open-iframe', window.parent.document).hide();
-    });
-    
+
 });
 
     
